bf6998c816c9b6c7f8ee48628212bf2b1d0b9762
Arthur63 <Arthur63@163.com>
2024-01-05 14:25:16 +0800

    remove unused files

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const Application = require('@waline/vercel');
 
 module.exports = Application({
   plugins: [],
-  async postSave(comment) {
-    // do what ever you want after comment saved
-  },
+  async postSave(comment) { },
 });
